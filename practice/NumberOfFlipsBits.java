bfffadc5e7427abef747b3c00ea2da8621a10630
karthik <karthik.parthasarathy@freshworks.com>
2022-10-02 17:15:54 +0530

    NumberOfFlipBits

---
+++ practice/NumberOfFlipsBits.java
@@ -0,0 +1,23 @@
+package practice;
+
+import java.util.Scanner;
+
+public class NumberOfFlipsBits {
+    public static int conversion(long A, long B) {
+        int cnt = 0;
+        long xor = A ^ B;
+        while (xor != 0) {
+            cnt++;
+            xor &= (xor - 1);
+        }
+        return cnt;
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        System.out.println(conversion(a, b));
+    }
+}
