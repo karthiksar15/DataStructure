f52900ec0c5e4e6fd6521c130fcad9b9d0503e0d
karthik <karthik.parthasarathy@freshworks.com>
2022-10-02 16:40:38 +0530

    ReverseBits & TwoNumbersNonRepeating

---
+++ practice/ReverseBits.java
@@ -0,0 +1,62 @@
+package practice;
+
+import java.util.Scanner;
+
+public class ReverseBits {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        long t = input.nextLong();
+        while (t-- > 0) {
+            long num = input.nextLong();
+            long ans = reverseBits(num);
+            System.out.print(ans);
+        }
+
+    }
+
+    public static long reverseBits(long num) {
+        long rev = 0;
+        int count = 32;
+        while (count > 0) {
+            rev <<= 1;
+            if ((num & 1) == 1)
+                rev ^= 1;
+            num >>= 1;
+            count--;
+        }
+        return rev;
+
+    }
+}
+
+
+//    Problem Description
+//    Reverse the bits of a given 32 bits unsigned integer.
+//
+//        Input format
+//        First line contains the number of test cases.
+//
+//        Each line contains a 32 bit unsigned integer.
+//
+//        Output format
+//        Output the Reversed unsigned integer.
+//
+//        Sample Input 1
+//        1
+//
+//        43261596
+//
+//        Sample Output 1
+//        964176192
+//
+//        Explanation 1
+//        The input binary string 00000010100101000001111010011100 represents the unsigned integer 43261596, so return 964176192 whose binary representation is 00111001011110000010100101000000.
+//
+//
+//        Constraints
+//        1 <= T <= 10^5
+//
+//        0 <= N <= 2^32-1
+//
+//        Resource
+//        Reverse bits of a given number
---
+++ practice/TwoNumbersNonRepeating.java
@@ -0,0 +1,78 @@
+package practice;
+
+import java.util.Scanner;
+
+public class TwoNumbersNonRepeating {
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        int ans[] = twoNonRepeatingNumbers(nums);
+        System.out.println(ans[0] + " " + ans[1]);
+        sc.close();
+    }
+
+    private static int[] twoNonRepeatingNumbers(int[] nums) {
+        int xor = 0, pos = 0, x = 0, y = 0;
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            xor = xor ^ nums[i];
+        }
+        for (int i = 0; i < 32; i++) {
+            if (((xor) & (1 << i)) > 0) {
+                pos = i;
+                break;
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (((nums[i]) & (1 << pos)) > 0) {
+                x = x ^ nums[i];
+            } else {
+                y = y ^ nums[i];
+            }
+        }
+        if (x < y) {
+            result[0] = x;
+            result[1] = y;
+        } else {
+            result[0] = y;
+            result[1] = x;
+        }
+        return result;
+    }
+
+}
+
+
+//    Problem Description
+//    Given an array in which all numbers except two are repeated once. (i.e. we have 2k+2 numbers and k numbers are occurring twice and remaining two have occurred once).
+//
+//        Find those two numbers in the most efficient way. Return the two numbers in a sorted manner.
+//
+//        Input format
+//        First line of input contains N which denotes the number of elements in the input array.
+//
+//        The next line of input contains N space separated integers.
+//
+//        Output format
+//        Return the two numbers which are present only once in the array in a sorted manner(i.e smaller number first).
+//
+//        Sample Input 1
+//        8
+//
+//        2 3 7 9 11 2 3 11
+//
+//        Sample Output 1
+//        7 9
+//
+//        Explanation
+//        All numbers except 7 and 9 are repeated once.
+//
+//        Constraints
+//        2<=N<=100000
+//
+//        1<=arr[i]<=100000
