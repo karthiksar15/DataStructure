a299dd30f88106d43722f9aef3b7f8ac7649d598
Karthik P <karthiksar15@gmail.com>
2023-09-03 21:02:17 +0530

    leetcode practice

---
+++ Practice_2/src/backtracking/Paranthesis.java
@@ -0,0 +1,40 @@
+package backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+public class Paranthesis {
+
+    public List<String> generateParenthesis(int n) {
+        List<String> answer = new ArrayList<>();
+        backtracking(answer, new StringBuilder(), 0, 0, n);
+
+        return answer;
+    }
+
+    private void backtracking(List<String> answer, StringBuilder curString, int leftCount, int rightCount, int n) {
+        if (curString.length() == 2 * n) {
+            answer.add(curString.toString());
+            return;
+        }
+        if (leftCount < n) {
+            curString.append("(");
+            backtracking(answer, curString, leftCount + 1, rightCount, n);
+            curString.deleteCharAt(curString.length() - 1);
+        }
+        if (leftCount > rightCount) {
+            curString.append(")");
+            backtracking(answer, curString, leftCount, rightCount + 1, n);
+            curString.deleteCharAt(curString.length() - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Paranthesis p=new Paranthesis();
+        System.out.println(p.generateParenthesis(3));
+
+
+    }
+}
